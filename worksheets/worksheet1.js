8e3e4b2e8369dd3e0b1a602337d41f36f1b27ea2
mstoel <148781788+mstoel@users.noreply.github.com>
2023-10-24 21:02:35 +0700

    Add files via upload

--- worksheets/worksheet1.js
+++ worksheets/worksheet1.js
@@ -34,10 +34,27 @@ document.addEventListener('DOMContentLoaded', () => {
                     doc.addImage(img, 'JPEG', 0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height);
 
                     exercises.forEach((exercise, index) => {
-                        const y = margin + index * verticalSpacing; // Adjust vertical spacing as needed.
+                        const y = margin + index * verticalSpacing;
                         const sentenceText = `${index + 1}. ${exercise.targetWord}\t\t${exercise.sentence}`;
-
-                        // Overlay text on top of the image
+                
+                        // Calculate the length of the sentence
+                        const sentenceLength = exercise.sentence.length;
+                
+                        // Define the font size range
+                        let fontSize = 16; // Default font size
+                        if (sentenceLength > 40) {
+                            // Reduce font size for longer sentences
+                            const extraCharacters = sentenceLength - 40;
+                            fontSize -= Math.floor(extraCharacters / 7) * 2; // Reduce 2px for every 10 characters
+                
+                            // Ensure the minimum font size is not too small
+                            if (fontSize < 10) {
+                                fontSize = 10;
+                            }
+                        }
+                
+                        // Overlay text on top of the image with the adjusted font size
+                        doc.setFontSize(fontSize);
                         doc.text(sentenceText, 20, y);
 
                         // Add a line under each sentence using a rectangle
@@ -45,12 +62,12 @@ document.addEventListener('DOMContentLoaded', () => {
                         const lineWidth = doc.internal.pageSize.width - 40; // Adjust as needed
                         doc.rect(20, lineY, lineWidth, 0.5, 'F');
 
+                    });
+
                         // Add the year and lesson information
                         doc.text(`${selectedYear}`, 150, 29);
                         doc.text(`/  ${selectedLesson}`, 170, 29);
-
-                    });
-
+                        
                     const pdfDataUri = doc.output('datauristring');
                     const pdfIframe = document.getElementById('pdfIframe');
                     pdfIframe.src = pdfDataUri;
